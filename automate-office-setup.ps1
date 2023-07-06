42e1258f9d518ce6e359e6b90b72420cff39f583
Pinak Prajapati <pinakprajapati333@gmail.com>
2023-07-05 17:38:05 -0700

    Add error handling to the script

--- automate-office-setup.ps1
+++ automate-office-setup.ps1
@@ -1,22 +1,36 @@
 #!/usr/bin/env pwsh
-# This script automates the office installtion and product license activation.
-Write-Host "Starting Office Installation..."
+# This script automates the Office installation and product license activation.
 
-# Step 1: Open command prompt as administrator
-Start-Process powershell -Verb RunAs
+try {
+    Write-Host "Starting Office Installation..."
 
-# Step 2: Navigate to Office 2021 setup folder in Command Line
-Set-Location -Path 'C:\Users\Administrator1\Desktop\Office2021'
+    # Step 1: Open command prompt as administrator
+    Start-Process powershell -Verb RunAs
 
-# Step 3: Download and Install Office 2021 using the specified configuration file
-Start-Process -FilePath 'setup.exe' -ArgumentList '/download', 'configuration-Office2021Enterprise.xml' -Wait
+    # Step 2: Navigate to Office 2021 setup folder in Command Line
+    Set-Location -Path 'C:\Users\Administrator1\Desktop\Office2021'
 
-# Step 4: Configure Office 2021 automatically using the specified configuration file
-Start-Process -FilePath 'setup.exe' -ArgumentList '/configure', 'configuration-Office2021Enterprise.xml' -Wait
+    # Step 3: Download and Install Office 2021 using the specified configuration file
+    Start-Process -FilePath 'setup.exe' -ArgumentList '/download', 'configuration-Office2021Enterprise.xml' -Wait
 
-Write-Host "Starting Office activation..."
+    # Step 4: Configure Office 2021 automatically using the specified configuration file
+    Start-Process -FilePath 'setup.exe' -ArgumentList '/configure', 'configuration-Office2021Enterprise.xml' -Wait
 
-# Step 5: Navigate to Office installation folder for license key
-Set-Location -Path 'C:\Program Files\Microsoft Office\Office16'
+    Write-Host "Office Installation completed successfully."
 
-# Further steps are taken automatically that activates the product licenses which are not added here for confidentiality.
+    Write-Host "Starting Office activation..."
+
+    # Step 5: Navigate to Office installation folder for license key
+    Set-Location -Path 'C:\Program Files\Microsoft Office\Office16'
+
+    # Further steps are taken automatically that activate the product licenses which are not added here for confidentiality.
+
+    Write-Host "Office activation completed successfully."
+}
+catch {
+    Write-Host "An error occurred during Office installation or activation:"
+    Write-Host $_.Exception.Message
+    exit 1
+}
+
+exit 0
